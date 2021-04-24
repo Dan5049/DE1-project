4f64ee1040e1c57c9baddcff494f412fbf647a72
Dan5049 <havranek.d@hotmail.cz>
2021-04-24 19:16:44 +0200

    scheme

--- parking_assistant/parking_assistant.srcs/sim_1/new/tb_top.vhd
+++ parking_assistant/parking_assistant.srcs/sim_1/new/tb_top.vhd
@@ -63,22 +63,22 @@ begin
    p_reset_gen : process
    begin   
         s_rst <= '1';
-        s_echo_i <= '0';
+        s_echo_i <= '1';
         wait for 100 ms;
         s_rst <= '0';
         wait;       
    end process p_reset_gen; 
          
-   p_echo_gen : process
-   begin
-        s_echo_i <= '1';
-        wait for 100 ms;
-        s_echo_i <= '0';
-        wait for 100 ms;
-        s_echo_i <= '1';
-        wait for 200 ms;
-        s_echo_i <= '0';
-        wait;
-  end process p_echo_gen;
+--   p_echo_gen : process
+--   begin
+--        s_echo_i <= '1';
+--        wait for 100 ms;
+--        s_echo_i <= '0';
+--        wait for 100 ms;
+--        s_echo_i <= '1';
+--        wait for 200 ms;
+--        s_echo_i <= '0';
+--        wait;
+--  end process p_echo_gen;
          
 end Behavioral;
